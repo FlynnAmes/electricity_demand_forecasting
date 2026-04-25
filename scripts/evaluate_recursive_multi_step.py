d4bd5e98b61bd15a63da470f8e9dbd0c169c4096
Flynn <F.ames@pgr.reading.ac.uk>
2026-04-25 17:09:20 +0100

    evaluate all models with recursive multi-timestep forecast

---
+++ scripts/evaluate_recursive_multi_step.py
@@ -0,0 +1,253 @@
+""" evaluate models doing multi-timestep forecast (using predictions to recursively forecast future time 
+    points) """
+
+import pandas as pd
+import pickle as pkl
+from sklearn.metrics import root_mean_squared_error
+import numpy as np
+import json
+from paths import DATA_PATH, MODELS_PATH, LOGS_PATH, CONFIG_PATH
+from glob import glob
+from pathlib import Path
+import os
+import yaml
+
+
+def unscale_per_client(group, dict_with_mean_and_std):
+    """ use mean and std usage for each client to unscale data, ready for 
+    evaluation """
+
+    client_id = int(np.unique(group.index.get_level_values('client_id'))[0])
+
+    return ((group * dict_with_mean_and_std[client_id]['std']) + dict_with_mean_and_std[client_id]['mean'])
+
+
+def log_stats(model_name: str, nrmse_per_client, nrmse_summary_dict, df_preds):
+    """ log performance metrics acrosss clients along with summary stats of these, and 
+    the predictions for future visualisation """
+
+    # define path to save and check that exists
+    save_path = LOGS_PATH / model_name / 'recursive'
+    os.makedirs(save_path, exist_ok=True)
+
+    with open(save_path / 'nrmse_summary_stats.json', 'w') as f:
+        json.dump(nrmse_summary_dict, f, indent=4)
+
+    with open(save_path / 'nrmse_per_client.json', 'w') as f:
+        # convert to list so serialisable
+        json.dump(nrmse_per_client.to_list(), f, indent=4)
+    
+    # save predictions (only) for further evaluation/visualisation (also gives client ids used)
+    os.makedirs(DATA_PATH / 'processed', exist_ok=True)
+    with open(DATA_PATH / 'processed' / f'{model_name}_preds_multistep.pkl', 'wb') as f:
+        pkl.dump(df_preds, f)
+
+
+
+def remove_first_week_and_up_to_first_6am_before_last_5am(g):
+    """ takes a group and removes the first week of data (naive assumption that all data included for now!)
+    and then data up to the first 6 am (first forecast step) """
+
+    # first remove first week
+    g_minus_1wk = g[168:]
+
+    # get min datetime that is 6am
+    datetimes = g_minus_1wk.index.get_level_values('datetime')
+    # client_id = g.index.get_level_values('client_id').unique()
+    min_6am = np.min(datetimes[datetimes.hour == 6])
+    max_6am = np.max(datetimes[datetimes.hour == 6])
+
+    # get index of min 6am and max 5am
+    idx_min_6am = g_minus_1wk.index.droplevel('client_id').get_loc(min_6am)
+    idx_max_6am = g_minus_1wk.index.droplevel('client_id').get_loc(max_6am)
+
+    return g_minus_1wk.iloc[idx_min_6am:idx_max_6am]
+
+
+
+def update_the_feature_array(initial_features_indexes, hour_index, col_idxs, X_test_array, y_test_array, y_preds_array, context_window=168):
+
+    """ take the index of data to collect and recompute lag and rolling features, ready for 
+    prediction (default context window of 168 steps (assuming 1 week for contigous data) """
+    
+    # get exisiting feature values at each initial forecast step (using single step
+    # engineered features as baseline) 
+    feature_array = X_test_array[initial_features_indexes + hour_index, :]
+
+    # expand dims for indexes so can broadcast later when want range of indexes
+    initial_features_indexes_2d = np.expand_dims(initial_features_indexes, axis=-1)
+
+    # use np.arange to extract contigous indexes (i.e., window; first axis) for each context window (zeroth axis)
+    data_from_target = y_test_array[initial_features_indexes_2d + hour_index - context_window + np.arange(context_window - hour_index)]
+    # then get data from preds
+    data_from_preds = y_preds_array[:, :hour_index]
+    # then combine
+    data_to_process = np.concat((data_from_target, data_from_preds), axis=-1)
+
+    # recompute lag features
+    feature_array[:, col_idxs['lag_1hr']] = data_to_process[:, -1]
+    feature_array[:, col_idxs['lag_2hr']] = data_to_process[:, -2]
+    feature_array[:, col_idxs['lag_6hr']] = data_to_process[:, -6]
+    feature_array[:, col_idxs['lag_1dy']] = data_to_process[:, -24]
+    feature_array[:, col_idxs['lag_1wk']] = data_to_process[:, -168]
+
+    # and the rolling features
+    feature_array[:, col_idxs['rolling_mean_1dy']] = np.mean(data_to_process[:, -24:], axis=-1)
+    feature_array[:, col_idxs['rolling_mean_1wk']] = np.mean(data_to_process[:, -168:], axis=-1)
+
+    feature_array[:, col_idxs['rolling_max_1dy']] = np.max(data_to_process[:, -24:], axis=-1)
+    feature_array[:, col_idxs['rolling_max_1wk']] = np.max(data_to_process[:, -168:], axis=-1)
+
+    feature_array[:, col_idxs['rolling_min_1dy']] = np.min(data_to_process[:, -24:], axis=-1)
+    feature_array[:, col_idxs['rolling_min_1wk']] = np.min(data_to_process[:, -168:], axis=-1)
+
+    return feature_array
+
+
+def get_forecast(model_object, X_test_array, y_test_array, initial_features_indexes, forecast_horizon, col_idxs, context_window=168):
+    """ step forwards the forecast in time to get predictions at each hour of day """
+
+    # initialise array for predictions (num_forecasts, hour_of_day)
+    y_preds_array = np.zeros((len(initial_features_indexes), forecast_horizon))
+
+    # loop over each hour to forecast (index zero starts at 6am)
+    for hour_index in range(forecast_horizon):
+
+        if hour_index == 0:
+            # if hour is zero, then just take the feature values from test data (already in correct order)
+            feature_array = X_test_array[initial_features_indexes]
+        else:
+            # otherwise concat exisiting observed data with predictions (recursive step)
+            feature_array = update_the_feature_array(initial_features_indexes=initial_features_indexes, 
+                                                    hour_index=hour_index, X_test_array=X_test_array, y_test_array=y_test_array, 
+                                                    col_idxs=col_idxs, y_preds_array=y_preds_array,
+                                                    context_window=context_window)
+        
+        # update the predictions matrix
+        y_preds_array[:, hour_index] = model_object.predict(feature_array)
+    
+    return y_preds_array
+
+
+
+def evaluate_models():
+
+    ###########
+    # get config params
+    ###########
+
+    with open(CONFIG_PATH, 'r') as f:
+        config = yaml.safe_load(f)
+
+    HORIZON = config['horizon']
+    CONTEXT_WINDOW = config['context_window']
+
+    ############
+    # load test data
+    ############
+
+    with open(DATA_PATH / 'processed' / 'df_tabular_test.pkl', 'rb') as f:
+        df_test = pkl.load(f)
+
+    # split into features and target
+    X_test = df_test.drop(columns=['hourly_usage_kwh'])
+    y_test = df_test['hourly_usage_kwh']
+
+    ############
+    # load in mean and std usages for each client.
+    ############
+
+    # for unscaling labels and predictions
+    with open(DATA_PATH / 'processed' / 'mean_std_per_client.json', 'r') as f:
+        df_std_mean_usage = pd.read_json(f).T
+        # create dictionary version for fast lookup
+        dict_std_mean_usage = df_std_mean_usage.to_dict(orient='index')
+    
+    ##########
+    # get indexes of predictions and columns, and convert to numpy
+    ##########
+
+    # get indexes for all timesteps (and correspinding client id) where a forecast prediction is to be made
+    indexes_of_all_predictions = X_test.groupby(level='client_id').apply(lambda g: remove_first_week_and_up_to_first_6am_before_last_5am(g)).index.droplevel(0)
+
+    # get datetimes (and corresponding client id) for starting step of each forecast horizon (i.e., 6am)
+    datetimes_forecast_start = indexes_of_all_predictions[indexes_of_all_predictions.get_level_values('datetime').hour == 6]
+
+    # get the numeric index for starting step of each forecast
+    initial_features_indexes = X_test.index.get_indexer(datetimes_forecast_start)
+
+    # get indexes for each column (ready for when convert to numpy)
+    col_idxs = {col: i for i, col in enumerate(X_test.columns)}
+
+    # get target values at the datetimes to be predicted
+    y_test_at_predictions = y_test[y_test.index.isin(indexes_of_all_predictions)]
+
+    # convert the test data to numpy
+    X_test_array = X_test.to_numpy()
+    y_test_array = y_test.to_numpy()
+
+    ##############
+    # main evaluation loop
+    ##############
+
+    # loop through each saved model, as well as two naive models, which just use features already computed
+    for path_name in glob(str(MODELS_PATH/ '*')):
+        
+        # if model is LSTM then skip as evaluation dealt with seperately
+        if 'LSTM' in path_name:
+            continue
+        # otherwise if not a file then skip
+        if os.path.isfile(path_name) is False:
+            continue
+        else:       
+            with open(path_name, 'rb') as f:
+                # get model name
+                model_name = Path(path_name).stem
+                # if model is _not_ naive, then load the model from the path
+                model = pkl.load(f)
+        
+        #############
+        # get predictions
+        ############
+        
+        # and fill in values
+        y_preds_array = get_forecast(model_object=model, X_test_array=X_test_array, y_test_array=y_test_array, 
+                                     initial_features_indexes=initial_features_indexes, forecast_horizon=HORIZON, 
+                                     col_idxs=col_idxs, context_window=CONTEXT_WINDOW)
+        
+        # create frame with predictions and labels for given client id
+        df_preds = pd.DataFrame(index=indexes_of_all_predictions, data={'y_pred': y_preds_array.flatten(),
+                                                        'y_true': y_test_at_predictions})
+        
+        # now unscale the predictions and labels to get original units
+        df_preds_unscaled = df_preds.groupby(level=
+                                        'client_id').transform(lambda g: 
+                                                                unscale_per_client(g, dict_std_mean_usage))
+        
+        # for each client compute the rmse
+        rmse_per_client = df_preds_unscaled.groupby(level=
+                                                    'client_id').apply(lambda g: 
+                                                                            root_mean_squared_error(g['y_true'], g['y_pred']))
+        
+        # normalise rmse by mean usage to make comparable across clients
+        nrmse_per_client = rmse_per_client/df_std_mean_usage['mean']
+
+        # create dict of summary stats
+        summary_dict = {
+                        'mean': nrmse_per_client.mean(),
+                        'std': nrmse_per_client.std(),
+                        'max': nrmse_per_client.max(),
+                        'min': nrmse_per_client.min(),
+                        'top_5_performing_clients': list(nrmse_per_client.sort_values().iloc[:5].index),
+                        'bottom_5_performing_clients': list(nrmse_per_client.sort_values(ascending=False).iloc[:5].index),
+                        }
+
+        # log summary stats
+        log_stats(model_name=model_name, nrmse_summary_dict=summary_dict, nrmse_per_client=nrmse_per_client, df_preds=df_preds_unscaled)
+
+        print(f'\n {model_name} predictions and performance metrics logged')
+
+
+if __name__ == '__main__':
+    evaluate_models()
+        
