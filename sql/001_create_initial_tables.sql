fd851bc6970b0dea3fc9e0417d2f5a5ee6e0f4ac
Flynn <F.ames@pgr.reading.ac.uk>
2026-04-29 10:53:07 +0100

    sql file for cleaning data for training, which wil also serve as actual values that will be piped into actuals table at each 24 hour interval for batch inference

--- sql/001_create_initial_tables.sql
+++ sql/001_create_initial_tables.sql
@@ -1,41 +1,34 @@
 -- Load raw data into a table. This way, schema are inferred automatically (with enforce later?)
 -- note that path assumes that running file from the db directory!
-CREATE TABLE raw_data AS 
+CREATE OR REPLACE TABLE raw_data AS 
     SELECT * FROM read_csv('../data/raw/LD2011_2014.txt', 
                             delim=';',
                             decimal_separator=',',
                             sample_size=-1);
 
-------------------------------------------------------
-
--- create table for cleaned data
-CREATE TABLE cleaned_data(
-
-client_id INTEGER,
-recorded_at DATETIME,
-hourly_usage DOUBLE,
-PRIMARY KEY (client_id, recorded_at)
-
-);
-
 -------------------------------------------------------
 
 -- table for raw inference data. Data will be appended to the table in daily intervals 
-CREATE TABLE raw_data_inference AS
+CREATE OR REPLACE TABLE raw_data_inference AS
     FROM raw_data
     LIMIT 0;
 
 ------------------------------------------------------
 
 -- table for cleaned inference data. Data will be appended to the table in daily intervals
-CREATE TABLE cleaned_data_inference AS
-    FROM cleaned_data
-    LIMIT 0;
+CREATE OR REPLACE  TABLE cleaned_hourly_usage_inference(
+
+client_id INTEGER,
+recorded_at DATETIME,
+hourly_usage DOUBLE,
+PRIMARY KEY (client_id, recorded_at) 
+
+);
 
 -------------------------------------------------------
 
 -- create table for features (generated from clean data)
-CREATE TABLE feature_store (
+CREATE OR REPLACE TABLE feature_store (
 
 client_id INTEGER,
 recorded_at DATETIME,
@@ -71,7 +64,7 @@ PRIMARY KEY (client_id, recorded_at)
 -------------------------------------------------
 
 -- contains predictions made by models
-CREATE TABLE predicted_usage (
+CREATE OR REPLACE TABLE predicted_hourly_usage (
 
 client_id INTEGER,
 recorded_at DATETIME,
@@ -84,7 +77,7 @@ PRIMARY KEY (client_id, recorded_at)
 --------------------------------------------------
 
 -- contains the actual values of usage (update as data is obtained each day)
-CREATE TABLE actual_usage (
+CREATE OR REPLACE TABLE actual_hourly_usage (
 
 client_id INTEGER,
 recorded_at DATETIME,
---
+++ sql/clean_data_for_training.sql
@@ -0,0 +1,80 @@
+-- Clean all the raw data, to create table of clean data that in this project, 
+-- will just form the 'actual' values that will be piped into teh actuals table at each 
+-- 24 hour interval (different to cleaning required in inference!)
+
+
+CREATE OR REPLACE TABLE cleaned_hourly_usage AS (
+-- CTE to melt the table, so that have a row for each timestamp and client id
+WITH melt_table AS (
+
+    UNPIVOT raw_data
+    ON  COLUMNS(* EXCLUDE column000)
+    INTO 
+        NAME client_id
+        VALUE usage
+),
+
+-- Then get hourly usage by averaging over three previous intervals
+-- Use CASE statement here to give nulls where not enough preceding rows for full hour
+
+-- Note that assumes complete 15 minute intervals (i.e, no missing intervals) for now
+get_hourly_usage AS (
+
+    SELECT  column000 AS recorded_at,
+            client_id,
+            CASE WHEN COUNT(*) OVER w = 4
+-- average usage over the hour will give the hourly usage in Kwh
+                 THEN AVG(usage) OVER w
+                 ELSE NULL
+            END AS hourly_usage
+
+    FROM melt_table
+    WINDOW w AS (
+                PARTITION BY client_id
+                ORDER BY recorded_at
+                ROWS BETWEEN 3 PRECEDING AND CURRENT ROW
+                )
+),
+
+-- get usage at hour only, to get hourly usage data only
+-- Note this effectively filters out Null values created by the earlier window operation
+-- Then also convert client id string to a integer with prefix removed
+get_usage_at_the_hour_and_convert_client_ids AS (
+
+    SELECT  recorded_at,
+            REPLACE(client_id, 'MT_', ''):: INTEGER AS client_id,
+            hourly_usage
+    FROM get_hourly_usage
+    WHERE DATE_PART('minute', recorded_at) = 0
+
+),
+
+-- remove data where not a customer, first get the cumulative usage
+get_cumu_usage AS (
+
+    SELECT *,
+    SUM(hourly_usage) OVER(PARTITION BY client_id
+             ORDER BY recorded_at
+             ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) as cumu_usage
+    FROM get_usage_at_the_hour_and_convert_client_ids
+),
+
+-- and remove where the cumulative usage is zero (assuming that not a customer at that point)
+remove_where_non_customer AS (
+
+    SELECT recorded_at,
+           client_id,
+           hourly_usage
+
+    FROM get_cumu_usage
+    WHERE cumu_usage > 1E-9
+)
+
+-- now finally just select all rows, so can create table using these
+SELECT * FROM remove_where_non_customer
+ORDER BY client_id, recorded_at
+
+);
+
+-- and explort to parquet file
+COPY cleaned_hourly_usage TO '../data/processed/hourly_usage_cleaned.parquet'
